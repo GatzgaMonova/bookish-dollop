d4906a5ac08129e36b1abcc32ee2417993623998
GatzgaMonova <114961624+GatzgaMonova@users.noreply.github.com>
2022-10-04 07:45:43 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
 a = float(input("What is the first number you would like to input?"))
 b = float(input("What is the second number you would like to input?"))
-operator = input("What operator would you like to use?")
 print("add, subtract, multiply, divide, exponentation")
+operator = input("What operator would you like to use?")
 
 if operator == "add":
-  print(a=b)
+  print(a+b)
 elif operator == "subtract":
   print(a-b)
 elif operator == "multiply":
